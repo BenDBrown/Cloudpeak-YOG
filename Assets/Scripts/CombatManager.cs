941d42168e438d9ac5ea576f01219c39a04c6aa6
BenDBrown <benjamin.d.brown.2002@gmail.com>
2023-03-25 22:19:23 +0100

    started on combat system
    turn ordering, stats, stat scaling, damaging attacks implemented. cannot yet use due to no UI and no enemy AI

--- Assets/Scripts/CombatManager.cs
+++ Assets/Scripts/CombatManager.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class CombatManager : MonoBehaviour
 {
+    // for checking if combat is enabled
+    private bool isFighting = false;
+
     // Assignables
     public PlayerPartyManager playerParty;
     public EnemyPartyManager enemyParty;
+    public ContainerManager enemy1;
+    public ContainerManager enemy2;
+    public ContainerManager enemy3;
+    public ContainerManager enemy4;
 
     // chance of a combat encounter
     public int percentEncounterChance = 100;
 
+    // lists for determining turn order
+    public List<float> toMoveList = new List<float>();
+    public List<float> hasMovedList = new List<float>();
+
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         int r = Random.Range(1, 101);
 
         if(r <= percentEncounterChance)
         {
             InitiateCombat();
+            isFighting = true;
             Debug.Log("LEEEEEEROY JEEENKINS");
         }
         else
@@ -31,16 +43,48 @@ public class CombatManager : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
+        if (isFighting == true)
+        {
+            CombatSetup();
+        }
         
+    }
+
+    void CombatSetup()
+    {
+        toMoveList.Add(GetComponentInChildren<Icarus>(false).speed);
+        toMoveList.Add(GetComponentInChildren<Magnus>(false).speed);
+        toMoveList.Add(GetComponentInChildren<Kena>(false).speed);
+        toMoveList.Add(GetComponentInChildren<Lysithea>(false).speed);
+        toMoveList.Add(enemy1.GetSpeed());
+        toMoveList.Add(enemy2.GetSpeed());
+        toMoveList.Add(enemy3.GetSpeed());
+        toMoveList.Add(enemy4.GetSpeed());
+        toMoveList.Sort();
+        /*
+        Debug.Log(toMoveList[0]);
+        Debug.Log(toMoveList[1]);
+        Debug.Log(toMoveList[2]);
+        Debug.Log(toMoveList[3]);
+        Debug.Log(toMoveList[4]);
+        Debug.Log(toMoveList[5]);
+        Debug.Log(toMoveList[6]);
+        Debug.Log(toMoveList[7]);
+        */
     }
 
     void InitiateCombat()
     {
         enemyParty.gameObject.SetActive(true);
+        
+    }
 
+    // will return the current floor number, right now returns a placeholder
+    public int GetFloor()
+    {
+        return 10;
     }
 
 }
